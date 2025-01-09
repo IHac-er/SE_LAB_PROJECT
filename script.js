7e80833b453b54f7c3c8c684cbd776911b216010
Tharun R <rtharun2012@gmail.com>
2025-01-10 00:43:14 +0530

    Initial empty commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Add interactivity to the "Click Me" button
+document.getElementById("hero-button").addEventListener("click", function() {
+    alert("Thanks for clicking! Have a great day!");
+});
