8235c1d1d3bdad9053272dc69ca3e0a7b6f82ba0
Tharun R <rtharun2012@gmail.com>
2025-02-26 21:22:38 +0530

    Added view-profile page to dashboard

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -7,3 +7,9 @@ def dashboard():
     if 'name' in session and 'email' in session:
         return render_template('dashboard/dashboard.html', name=session['name'], email=session['email'])
     return redirect('/login')  
+
+@dashboard_bp.route('/view-profile')
+def viewprofile():
+    if 'name' in session and 'email' in session:
+        return render_template('dashboard/viewprofile.html', name=session['name'], email=session['email'])
+    return redirect('/dashboard')  
